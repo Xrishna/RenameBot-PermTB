bdf5d3333cab5282a1743e0997e36daee8803bff
Krishna <82990200+Xrishna@users.noreply.github.com>
2021-06-26 11:50:05 +0530

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -23,7 +23,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "ㅤㅤㅤㅤ╭───────────────╮\nㅤㅤㅤㅤ│  📊  **Progress Bar**  📊\nㅤㅤㅤㅤㅤㅤ╰───────────────╯\n\n╭───────────────────────╮\n│ » 📊 **Status :** Downloding😴.....\n│\n│ » [{0}{1}]\n│\n│ » 📥 **Downloaded :** {2}%".format(
+        progress = "ㅤㅤㅤㅤ╭───────────────╮\n ㅤㅤㅤㅤ ㅤ│  📊  **Progress Bar**  📊\nㅤㅤㅤㅤㅤ╰───────────────╯\n\n╭───────────────────────╮\n│ » 📊 **Status :** Downloding😴.....\n│\n│ » [{0}{1}]\n│\n│ » 📥 **Downloaded :** {2}%".format(
             ''.join(["■" for i in range(math.floor(percentage / 5))]),
             ''.join(["▢" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
@@ -53,7 +53,7 @@ def humanbytes(size):
         return ""
     power = 2**10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
         n += 1
